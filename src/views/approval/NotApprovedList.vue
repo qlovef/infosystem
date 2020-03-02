76e15b9bc9494b6c4768d4a59032050df94ef759
text <1071121289@qq.com>
2020-03-02 17:43:37 +0800

    fleat:approved

--- src/views/approval/NotApprovedList.vue
+++ src/views/approval/NotApprovedList.vue
@@ -8,33 +8,62 @@
                 <el-table-column
                     prop="Name"
                     label="姓名"
-                    width="180">
+                    width="180"
+                    align="center">
                 </el-table-column>
                 <el-table-column
                     prop="JobNumber"
                     label="工号"
-                    width="180">
+                    width="180"
+                    align="center">
                 </el-table-column>
                 <el-table-column
                     prop="Type"
                     label="类型"
-                    width="180">
+                    width="180"
+                    align="center">
+                    <template slot-scope="scope">
+                        {{vacationType[scope.row.Type]}}
+                    </template>
+                </el-table-column>
+                <el-table-column
+                    prop=""
+                    label="请假原因"
+                    align="center">
                 </el-table-column>
                 <el-table-column
                     prop="StartTime"
-                    label="开始时间">
+                    label="开始时间"
+                    width="200"
+                    align="center">
                 </el-table-column>
                 <el-table-column
                     prop="EndTime"
-                    label="结束时间">
+                    label="结束时间"
+                    width="200"
+                    align="center">
                 </el-table-column>
                 <el-table-column
                     prop="IsPermit"
                     label="是否审批"
-                    width="180">
+                    width="180"
+                    align="center">
                     <template slot-scope="scope">
-                        {{scope.row.IsPermit}}
+                       <span
+                            :class="{
+                                'success': scope.row.IsPermit === 1,
+                                'error': scope.row.IsPermit === 2
+                            }">
+                            {{ scope.row.IsPermit === 1 ? '已批准' :'未审批' }}
+                        </span>
                     </template>
+                </el-table-column>
+                <el-table-column
+                    prop=""
+                    label=""
+                    width="180"
+                    align="center">
+                    <div>意见</div>
                 </el-table-column>
             </el-table>
         </div>
@@ -68,8 +97,44 @@ export default class NotApprovedList extends Vue {
         PageCount: 0,
         TotalCount: 0
     }
+
+    // 搜索信息
+    searchInfo = ''
+    // 请假类型
+    vacationType = [
+        '事假',
+        '病假',
+        '婚假',
+        '孕假',
+        '年假',
+        '调休假'
+    ]
+
+    created () {
+        this.fetchNotApprovedRecordList()
+    }
+    // 改变页码的时候触发
+    onPageChange (page: number) {
+        this.pagination.Page = page
+        this.fetchNotApprovedRecordList()
+    }
+
+    async fetchNotApprovedRecordList () {
+        const result: any = await RequestGetNotApprovedRecordList({
+            Page: this.pagination.Page,
+            PageSize: this.pagination.PageSize,
+            Name: this.searchInfo
+        })
+        if (result.Code === 1) {
+            this.approvedRecordList = result.Data.List
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.table-style {
+    width: 100%;
+    text-align: center;
+}
 </style>
